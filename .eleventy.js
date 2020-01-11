ea6cef29d3978e0df6fd03f9f8c7722984db6b60
Karlo <karlo.bistricki256@gmail.com>
2020-01-11 13:46:58 +0100

    Migrated to eleventy-navigation for navbar

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("source/styles");
 	eleventyConfig.addPassthroughCopy("source/images");
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
 	return {
 		dir: {
