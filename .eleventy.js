7db8cc37d8b1a5c7489519abffcfd575497b8113
Karlo <karlo.bistricki256@gmail.com>
2020-01-12 18:32:28 +0100

    Fixed script support for layouts (it actually works now)

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("source/styles");
 	eleventyConfig.addPassthroughCopy("source/images");
+	eleventyConfig.addPassthroughCopy("source/scripts");
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
 	return {
