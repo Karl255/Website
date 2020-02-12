38aff80a82102aec03de8d69e8989778d7cf2815
Karl255 <karlo.bistricki256@gmail.com>
2020-02-12 14:35:25 +0100

    Removed obfuscation from rm-8-script.js

--- source/scripts/rm-8-script.js
+++ source/scripts/rm-8-script.js
@@ -1,8 +1,27 @@
 function shortenAddress() {
-	let a = document.getElementById("longAddress").value.split(":").map(x => parseInt(x, 16));
-	let s = 0;
-	let e = -1;
-	for (let i = 0; i < 8; ++i) if (a[i] === 0) for (let t = i; i < 8 && a[i + 1] === 0; ++i && i + 1 - t > e - s && (s = t) + 1 && (e = i + 1));
-	a.splice((s == 0 && !(a[0] = "") && s++) + 1 && (e == 8 && !(a[7] = "") && e--) + 1 ? s : e, e - s, "");
-	document.getElementById("shortAddress").value = a.map(x => x.toString(16)).join(":");
+	let address = document.getElementById("longAddress").value.split(":").map(x => parseInt(x, 16));
+	let start = 0;
+	let end = -1;
+	for (let i = 0; i < 8; i++) {
+		if (address[i] === 0) {
+			while (i < 8 && address[i + 1] === 0) {
+				let temp = i;
+				if (i + 1 - temp > end - start) {
+					start = temp;
+					end = i + 1;
+				}
+				i++;
+			}
+		}
+	}
+	if (start == 0) {
+		address[0] = "";
+		start++;
+	}
+	if (end == 8) {
+		address[7] = "";
+		end--;
+	}
+	address.splice(start, end - start, "");
+	document.getElementById("shortAddress").value = address.map(x => x.toString(16)).join(":");
 }
