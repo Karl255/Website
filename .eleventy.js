22c8e0d1335897d37978e4e1bbd23e5a34b7f98b
Karl255 <karlo.bistricki256@gmail.com>
2020-04-27 20:52:21 +0200

    Added passtrough config for jpg, jpeg and png

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("source/styles");
 	eleventyConfig.addPassthroughCopy("source/images");
 	eleventyConfig.addPassthroughCopy("source/scripts");
+	eleventyConfig.addPassthroughCopy("source/sjiwp/**/*.jpg");
+	eleventyConfig.addPassthroughCopy("source/sjiwp/**/*.jpeg");
+	eleventyConfig.addPassthroughCopy("source/sjiwp/**/*.png");
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
 	return {
