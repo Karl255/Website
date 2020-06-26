4ad476cd530926c75fb87ae049595bfc137b853f
Karl255 <karlo.bistricki256@gmail.com>
2020-06-26 18:03:50 +0200

    Refactored names

--- source/scripts/navigation-bar.js
+++ source/scripts/navigation-bar.js
@@ -1,5 +1,5 @@
 function updateDebugNav() {
-	document.getElementsByClassName("debug-nav")[0].style.display =
+	document.getElementsByClassName("debug-navigation")[0].style.display =
 		sessionStorage.getItem("debugMode") === "false"
 		? "none"
 		: "inline-block";
