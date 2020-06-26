871f48770d46251d444da9afe06a5dc750f90aa2
Karl255 <karlo.bistricki256@gmail.com>
2020-06-26 21:26:48 +0200

    Refactored and simplified navigation bar

--- source/scripts/navigation-bar.js
+++ source/scripts/navigation-bar.js
@@ -1,20 +1,23 @@
+//show or hide debug nav
 function updateDebugNav() {
-	document.getElementsByClassName("debug-navigation")[0].style.display =
-		sessionStorage.getItem("debugMode") === "false"
-		? "none"
-		: "inline-block";
+	let el = document.getElementsByClassName("debug-navigation")[0];
+	if (sessionStorage.debugMode === "true")
+		el.style = ""; //remove inline style
+	else
+		el.style.display = "none"; //add inline style to hide it
 }
 
-function toggleDebugNav() {
-	sessionStorage.setItem("debugMode",
-		sessionStorage.getItem("debugMode") === "false"
-		? "true"
-		: "false");
+//button event handler
+//inverts value in session storage and then updates debug nav element
+function toggleDebugMode() {
+	sessionStorage.debugMode = sessionStorage.debugMode === "false" ? "true" : "false";
 	updateDebugNav();
 }
 
+//if unset, set
 if (sessionStorage.getItem("debugMode") === null) {
 	sessionStorage.setItem("debugMode", "false");
 }
 
+//update debug nav on page load
 updateDebugNav();
