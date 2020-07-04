17404c54b9d8e7eb9455d3273fa52fd0710f139e
Karlo <49595438+Karl255@users.noreply.github.com>
2020-07-04 23:24:44 +0200

    Start migrate to Statiq.Web

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace Website
+{
+	class Program
+	{
+		static async Task<int> Main(string[] args) =>
+			await Bootstrapper
+			.Factory
+			.CreateWeb(args)
+			//.DeployToNetlify(Config.FromSetting("NETLIFY_WEBSITE_ID"), Config.FromSetting("NETLIFY_TOKEN"))
+			.RunAsync();
+	}
+}
---
+++ SiteKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace Website
+{
+	public static class SiteKeys
+	{
+		public const string AdditionalStyles = nameof(AdditionalStyles);
+		public const string AdditionalScripts = nameof(AdditionalScripts);
+	}
+}
