b79789fb0be08631126bb44ed562fb5ce9fc6b72
Karlo <49595438+Karl255@users.noreply.github.com>
2021-11-01 01:50:06 +0100

    Expanded abaout-me and more - some small color and theme stuff - decortions for centered h1 elements - replaced smart code with dumb code, because chrome implemented calc in the wrong way - (arguably) better heading sizes - a heart-marker class just for WSL ♥

--- src/routes/about-me.svelte
+++ src/routes/about-me.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <section class="columns three-columns">
-	<h1>What I do:</h1>
+	<h1>What I do</h1>
 	<section>
 		<h2>Programming</h2>
-		<p>Primarily C#, but I also know C, Lua and JavaScript</p>
+		<p>Primarily C#, but I also know a couple other and I am looking to learn more</p>
 	</section>
 	<section>
 		<h2>Design</h2>
@@ -16,6 +16,72 @@
 	<section>
 		<h2>Electronics</h2>
 		<p>Mostly a hobby, for fun, but I know a fair bit</p>
+	</section>
+</section>
+
+<section class="columns two-columns">
+	<h1>What I know</h1>
+	<section>
+		<h2>Languages</h2>
+		<ul>
+			<li>C# &minus; currently my main go-to language</li>
+			<li>JavaScript</li>
+			<li>Lua</li>
+			<li>C</li>
+		</ul>
+		<ul>
+			<li>CSS</li>
+			<li>SCSS/Sass</li>
+			<li>Less</li>
+		</ul>
+	</section>
+	<section>
+		<h2>Tooling</h2>
+		<ul>
+			<li>Visual Studio</li>
+			<li>VS Code (and forks)</li>
+			<li class="heart-marker">WSL</li>
+			<li>git</li>
+			<li>keyboard, occasionally also a mouse</li>
+		</ul>
+	</section>
+	<section>
+		<h2>Platforms and such</h2>
+		<ul>
+			<li>The web</li>
+			<li>Cross-platform CLI tools</li>
+			<li>Desktop GUI apps (only for Windows; awaiting MAUI)</li>
+			<li>PlatformIO, Arduino</li>
+			<li>MonoGame</li>
+		</ul>
+	</section>
+</section>
+
+<section class="columns three-columns">
+	<h1>My projects</h1>
+	<section>
+		<h2><a href="https://github.com/Karl255/UserCSS-Styles">UserCSS-Styles</a></h2>
+		<p>A couple UserCSS styles I made and published to make the web prettier for me and others as well.</p>
+	</section>
+	<section>
+		<h2><a href="https://github.com/Karl255/NBodySim">NBodySim</a></h2>
+		<p>A 2D n-body physics simulator with honestly quite unintuitive controls.</p>
+	</section>
+	<section>
+		<h2><a href="https://github.com/Karl255/YetAnotherTetrisClone">YetAnotherTetrisClone</a></h2>
+		<p>The name says everything. A very very simple clone in MonoGame.</p>
+	</section>
+	<section>
+		<h2><a href="https://github.com/Karl255/WeatherFetch">WeatherFetch</a></h2>
+		<p>A CLI utility for getting the current weather. I created it to learn more about how to make CLI tools, implement multi-API backends and properly use dependency injection.</p>
+	</section>
+	<section>
+		<h2><a href="https://github.com/Karl255/ArduinoStaticWebServer">ArduinoStaticWebServer</a></h2>
+		<p>A very basic and limited static web server for Arduino. Serves pages from a FAT32 formatted SD card... slowly... very slowly...</p>
+	</section>
+	<section>
+		<h2><a href="https://github.com/Karl255/Nesk">Nesk</a></h2>
+		<p>My attempt at a cycle-accurate, cross-platform NES emulator. Doesn't work, don't bother with it. This was my HS graduation project.</p>
 	</section>
 </section>
 
