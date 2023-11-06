6d448620f3dc1c429856daa371fefbbd98f4baf6
Karl255 <49595438+Karl255@users.noreply.github.com>
2023-11-06 12:07:44 +0100

    Finished migration to newest SvelteKit

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
+import adapter from "@sveltejs/adapter-static"
+import preprocess from "svelte-preprocess";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-import preprocess from "svelte-preprocess";
-import staticAdapter from "@sveltejs/adapter-static"
-/** @type {import('@sveltejs/kit').Config} */
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
     "extensions": [".svelte", ...mdsvexConfig.extensions],
 
+	preprocess: [preprocess({}), mdsvex(mdsvexConfig)],
+
     kit: {
-		adapter: staticAdapter()
+		adapter: adapter()
 	},
 
-    preprocess: [preprocess({}), mdsvex(mdsvexConfig)]
 };
 
 export default config;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+// import type { UserConfig } from "vite";
+
+const config = {
+	plugins: [sveltekit()],
+};
+
+export default config;
